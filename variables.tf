9859c5f1fbd13e51737692919015bbc1ec988c24
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-14 21:17:17 -0500

    added basic terraform validator and main.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud Platform project"
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+}
+
