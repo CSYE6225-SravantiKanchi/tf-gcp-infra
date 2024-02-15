4e71a8ac040f28ce15c05a3751d425ce8ecf1fe0
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-15 09:20:48 -0500

    added subnets and a webapp specific route

--- variables.tf
+++ variables.tf
@@ -6,12 +6,23 @@ variable "region" {
   description = "The region where the resources will be created"
 }
 variable "vpc_config" {
-  type = map
+  type        = map(any)
   description = "The VPC Configuration"
   default = {
     delete_default_routes_on_create = true
-    auto_create_subnetworks = false
-    routing_mode            = "REGIONAL"
+    auto_create_subnetworks         = false
+    routing_mode                    = "REGIONAL"
   }
 }
 
+variable "subnets" {
+  type = list(object({
+    name          = string
+    ip_cidr_range = string
+  }))
+}
+
+variable "webapp_route" {
+  type        = map(any)
+  description = "The webapp route configuration"
+}
